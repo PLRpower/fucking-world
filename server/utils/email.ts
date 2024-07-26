eb1bceee10ae0b361c48743ec05f37bc1144d183
plr <paul.thomas.stras@gmail.com>
2024-07-26 11:13:13 +0200

    Correction pour l'obtention de l'email

--- server/utils/email.ts
+++ server/utils/email.ts
@@ -1,7 +1,7 @@
-// server/utils/email.ts
 import Stripe from 'stripe';
 import nodemailer from 'nodemailer';
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: 465,
@@ -14,13 +14,21 @@ const transporter = nodemailer.createTransport({
 
 export async function sendEmail(paymentIntent: Stripe.PaymentIntent) {
 
+    const customer = await stripe.customers.retrieve(paymentIntent.customer as string);
+    if (isDeletedCustomer(customer)) {return}
+
     const emailData = {
         from: '"F*cking World" <contact@osalys.studio>',
-        to: paymentIntent.receipt_email!,
+        to: customer.email!,
         subject: "Réservation confirmée pour l'évènement F*cking World",
-        text: `Merci pour votre inscription d'un montant de ${paymentIntent.amount_received / 100}.`,
-        html: `<p>Merci pour votre inscription d'un montant de ${paymentIntent.amount_received / 100}.</p>`
+        text: `Merci pour votre inscription d'un montant de ${paymentIntent.amount_received / 100}€.`,
+        html: `<p>Merci pour votre inscription d'un montant de ${paymentIntent.amount_received / 100}€.</p>`
     };
 
     await transporter.sendMail(emailData);
+
+}
+
+function isDeletedCustomer(customer: Stripe.Customer | Stripe.DeletedCustomer): customer is Stripe.DeletedCustomer {
+    return (customer as Stripe.DeletedCustomer).deleted !== undefined;
 }
